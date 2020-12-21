85a4e8fac372e4f47e00235c53c17673f4fd82f6
298 <652298784@qq.com>
2020-12-21 11:20:25 +0800

    first

---
+++ config/config.inc.php
@@ -0,0 +1,14 @@
+<?php 
+ return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => 'edulineccc',
+  'DB_USER' => 'root',
+  'DB_PWD' => '',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'el_',
+  'DB_CHARSET' => 'utf8',
+  'SECURE_CODE' => '1527956ebb69d9b780',
+  'COOKIE_PREFIX' => 'el_',
+); 
+ ?>
